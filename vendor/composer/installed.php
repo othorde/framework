71bbfd29f4fefc5f40f0a78669daea069fa95a2b
Oliver Thordeman <o.thordeman@live.se>
2021-05-06 08:37:14 +0100

    kmom04 framework

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c107e98629225ceda82305800ce3ea06a5cde62e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c107e98629225ceda82305800ce3ea06a5cde62e',
+    ),
+  ),
+);
